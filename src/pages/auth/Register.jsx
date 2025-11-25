909adcb9dfb613846c66826dadc76a58e57df5f1
salahdine <salahdaha7@gmail.com>
2025-11-25 10:10:10 +0100

    feat : refactore le design de register and login

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -242,8 +242,6 @@ const Register = () => {
                             </div>
                         </div>
 
-            
-
                         {/* Sign In Link */}
                         <p className="text-center text-sm text-gray-600 mt-6">
                             Already have an account?{' '}
